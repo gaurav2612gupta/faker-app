64cca216d1855e4568d8b052ad6ff30954e2dba8
Gaurav Gupta <gaurav1122gupta@gmail.com>
2022-09-11 03:04:19 +0530

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="fakerApp" element={<Home />} />
           <Route path="search" element={<Search />} />
       </Routes>
     </BrowserRouter>
