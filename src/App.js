5bb78493c339f20c9672be8b219be1cad03edf7b
Gaurav Gupta <gaurav1122gupta@gmail.com>
2022-09-11 19:06:36 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Search from "./pages/Search"
 import './pages/index.css'
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
-      <Routes>
-          <Route exact path="fakerApp" element={<Home />} />
-          <Route exact path="search" element={<Search />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+          <Route path="/" component = {Home} exact />
+          <Route path="/search" component = {Search} exact />
+    </div>
   )
 }
 
